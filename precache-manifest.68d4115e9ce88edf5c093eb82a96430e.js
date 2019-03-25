6cb940235032b3a1b36deac7693716d03141654a
cyanheart <zhenghanc@gmail.com>
2019-03-25 21:07:38 +0800

    commit

---
+++ precache-manifest.68d4115e9ce88edf5c093eb82a96430e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbdcf7a4e3e56a8cdca5",
+    "url": "/cyanheart/badminton.github.io/static/js/runtime~main.735a38b7.js"
+  },
+  {
+    "revision": "5af6cebcc7f4b6a9a377",
+    "url": "/cyanheart/badminton.github.io/static/js/main.d41ce1f3.chunk.js"
+  },
+  {
+    "revision": "1ca381b63592fc63901d",
+    "url": "/cyanheart/badminton.github.io/static/js/2.fbc3a94c.chunk.js"
+  },
+  {
+    "revision": "5af6cebcc7f4b6a9a377",
+    "url": "/cyanheart/badminton.github.io/static/css/main.42d744bc.chunk.css"
+  },
+  {
+    "revision": "1ca381b63592fc63901d",
+    "url": "/cyanheart/badminton.github.io/static/css/2.3328c3ed.chunk.css"
+  },
+  {
+    "revision": "03d1fa1c5ab5d25759b24c35d8ef8891",
+    "url": "/cyanheart/badminton.github.io/index.html"
+  }
+];
